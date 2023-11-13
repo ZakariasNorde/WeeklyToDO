24de734b157b0caffdbed5054e381bb6f31c9922
ZakariasNorde <nordeborgz@gmail.com>
2023-11-13 13:54:25 +0100

    Design week

--- ToDoCalender/ToDoCalender/Form1.Designer.cs
+++ ToDoCalender/ToDoCalender/Form1.Designer.cs
@@ -28,12 +28,270 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblWeek = new Label();
+            lblSunday = new Label();
+            lblTuesday = new Label();
+            lblSaturday = new Label();
+            lblFriday = new Label();
+            lblThursday = new Label();
+            lblWednesday = new Label();
+            lblMonday = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            pictureBox9 = new PictureBox();
+            pictureBox10 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            SuspendLayout();
+            // 
+            // lblWeek
+            // 
+            lblWeek.AutoSize = true;
+            lblWeek.BackColor = SystemColors.Highlight;
+            lblWeek.Location = new Point(575, 9);
+            lblWeek.Name = "lblWeek";
+            lblWeek.Size = new Size(72, 20);
+            lblWeek.TabIndex = 1;
+            lblWeek.Text = "Week.........";
+            // 
+            // lblSunday
+            // 
+            lblSunday.AutoSize = true;
+            lblSunday.BackColor = SystemColors.Info;
+            lblSunday.Location = new Point(1067, 45);
+            lblSunday.Margin = new Padding(0);
+            lblSunday.Name = "lblSunday";
+            lblSunday.Size = new Size(57, 20);
+            lblSunday.TabIndex = 7;
+            lblSunday.Text = "Sunday";
+            lblSunday.Click += lblSunday_Click;
+            // 
+            // lblTuesday
+            // 
+            lblTuesday.AutoSize = true;
+            lblTuesday.BackColor = SystemColors.ActiveCaption;
+            lblTuesday.Location = new Point(207, 43);
+            lblTuesday.Name = "lblTuesday";
+            lblTuesday.Size = new Size(67, 20);
+            lblTuesday.TabIndex = 2;
+            lblTuesday.Text = " Tuesday";
+            lblTuesday.Click += lblTuesday_Click;
+            // 
+            // lblSaturday
+            // 
+            lblSaturday.AutoSize = true;
+            lblSaturday.BackColor = SystemColors.Info;
+            lblSaturday.Location = new Point(890, 45);
+            lblSaturday.Name = "lblSaturday";
+            lblSaturday.Size = new Size(67, 20);
+            lblSaturday.TabIndex = 6;
+            lblSaturday.Text = "Saturday";
+            // 
+            // lblFriday
+            // 
+            lblFriday.AutoSize = true;
+            lblFriday.BackColor = SystemColors.ActiveCaption;
+            lblFriday.Location = new Point(724, 45);
+            lblFriday.Name = "lblFriday";
+            lblFriday.Size = new Size(49, 20);
+            lblFriday.TabIndex = 5;
+            lblFriday.Text = "Friday";
+            // 
+            // lblThursday
+            // 
+            lblThursday.AutoSize = true;
+            lblThursday.BackColor = SystemColors.ActiveCaption;
+            lblThursday.Location = new Point(533, 43);
+            lblThursday.Name = "lblThursday";
+            lblThursday.Size = new Size(68, 20);
+            lblThursday.TabIndex = 4;
+            lblThursday.Text = "Thursday";
+            lblThursday.Click += label5_Click;
+            // 
+            // lblWednesday
+            // 
+            lblWednesday.AutoSize = true;
+            lblWednesday.BackColor = SystemColors.ActiveCaption;
+            lblWednesday.Location = new Point(361, 45);
+            lblWednesday.Name = "lblWednesday";
+            lblWednesday.Size = new Size(85, 20);
+            lblWednesday.TabIndex = 3;
+            lblWednesday.Text = "Wednesday";
+            // 
+            // lblMonday
+            // 
+            lblMonday.AutoSize = true;
+            lblMonday.BackColor = SystemColors.ActiveCaption;
+            lblMonday.Location = new Point(46, 45);
+            lblMonday.Name = "lblMonday";
+            lblMonday.Size = new Size(63, 20);
+            lblMonday.TabIndex = 1;
+            lblMonday.Text = "Monday";
+            lblMonday.Click += label2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.Window;
+            pictureBox1.Location = new Point(155, 43);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(10, 474);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = SystemColors.Window;
+            pictureBox2.Location = new Point(317, 43);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(10, 474);
+            pictureBox2.TabIndex = 9;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = SystemColors.Window;
+            pictureBox3.Location = new Point(479, 43);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(10, 474);
+            pictureBox3.TabIndex = 10;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = SystemColors.Window;
+            pictureBox4.Location = new Point(652, 43);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(10, 474);
+            pictureBox4.TabIndex = 11;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = SystemColors.Window;
+            pictureBox5.Location = new Point(834, 43);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(10, 474);
+            pictureBox5.TabIndex = 12;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = SystemColors.Window;
+            pictureBox6.Location = new Point(1004, 43);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(10, 474);
+            pictureBox6.TabIndex = 13;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = SystemColors.Window;
+            pictureBox7.Location = new Point(1177, 43);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(10, 474);
+            pictureBox7.TabIndex = 14;
+            pictureBox7.TabStop = false;
+            pictureBox7.Click += pictureBox7_Click;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = SystemColors.Window;
+            pictureBox8.Location = new Point(0, 32);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(1187, 10);
+            pictureBox8.TabIndex = 15;
+            pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.BackColor = SystemColors.Window;
+            pictureBox9.Location = new Point(0, 43);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(10, 474);
+            pictureBox9.TabIndex = 16;
+            pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.BackColor = SystemColors.Window;
+            pictureBox10.Location = new Point(0, 68);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(1187, 10);
+            pictureBox10.TabIndex = 17;
+            pictureBox10.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(1221, 519);
+            Controls.Add(pictureBox10);
+            Controls.Add(pictureBox9);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblWeek);
+            Controls.Add(lblTuesday);
+            Controls.Add(lblWednesday);
+            Controls.Add(lblMonday);
+            Controls.Add(lblThursday);
+            Controls.Add(lblFriday);
+            Controls.Add(lblSunday);
+            Controls.Add(lblSaturday);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label lblWeek;
+        private Label lblSunday;
+        private Label lblTuesday;
+        private Label lblSaturday;
+        private Label lblFriday;
+        private Label lblThursday;
+        private Label lblWednesday;
+        private Label lblMonday;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox10;
     }
 }
